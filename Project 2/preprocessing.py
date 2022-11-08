106d2e34becde9b3c2f4105bf077c06698b2a68c
Han Hua <hanhuateo@gmail.com>
2022-11-08 22:29:55 +0800

    Database class in preprocessing

--- Project 2/preprocessing.py
+++ Project 2/preprocessing.py
@@ -2,6 +2,16 @@ import psycopg2
 import queue
 from configparser import ConfigParser
 
-class database:
+class Database:
     def __init__(self, host="localhost", port=5432, database="TPC-H", user="postgres", password="database"):
         self.conn = psycopg2.connect(host=host, port=port, database=database)
+        self.cur = self.conn.cursor()
+
+    def execute(self, query):
+        self.cur.execute(query)
+        query_results = self.cur.fetchall()
+        return query_results
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
