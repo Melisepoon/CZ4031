b526bb3823753146a7133986100c35c3506d1fe3
Han Hua <hanhuateo@gmail.com>
2022-11-12 13:48:13 +0800

    connect to db

--- Project 2/preprocessing.py
+++ Project 2/preprocessing.py
@@ -15,3 +15,41 @@ class Database:
     def close(self):
         self.cur.close()
         self.conn.close()
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename,))
+    
+    return db
+
+def connect():
+    conn = None
+    try:
+        params = config()
+
+        print('connecting to postgresql database')
+        conn = psycopg2.connect(**params)
+
+        cur = conn.cursor()
+
+        print('postgresql database version:')
+        cur.execute('SELECT version()')
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection clsoed')
